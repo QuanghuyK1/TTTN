761f21209812ee9de0c4fa5fed9b245596d4f2f5
QuanghuyK1 <87079310+QuanghuyK1@users.noreply.github.com>
2023-09-26 20:46:57 +0700

    update full

---
+++ TTTN/WEB_TTTN/Hospital_Application/Dialogs/AdminMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospital_Application.Dialogs
+{
+    public partial class AdminMenu : Form
+    {
+        public AdminMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LocationDialog dialog = new LocationDialog();
+            dialog.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TypeServiceDialog dialog = new TypeServiceDialog();
+            dialog.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ClassesDialog dialog = new ClassesDialog();
+            dialog.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ScheduleEmpAdmin dialog = new ScheduleEmpAdmin();
+            dialog.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AdminEmp adminEmp = new AdminEmp();
+            adminEmp.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Menu m = new Menu();
+            m.Show();
+        }
+    }
+}
---
+++ TTTN/WEB_TTTN/WEB_TTTN/Helpers/AutoIncre.cs
@@ -0,0 +1,106 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WEB_TTTN.Entities;
+using System.Linq;
+using System.Threading.Tasks; // Add this namespace for asynchronous operations
+
+namespace WEB_TTTN.Helpers
+{
+    public class AutoIncre
+    {
+        private readonly HospitalDatabaseContext _context;
+
+        public AutoIncre(HospitalDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<string> GetNextIdAsync() // Mark it as async and return Task<string>
+        {
+            var matchingIds = await _context.HospitalHealthInsurances
+                .Select(entity => entity.InsuranceId)
+                .ToListAsync(); // Remove the extra dots here
+
+            var highestId = matchingIds
+                .Where(id => id.StartsWith("ABC-"))
+                .DefaultIfEmpty("ABC-000000")
+                .Max();
+
+            var nextId = IncrementId(highestId);
+
+            return nextId;
+        }
+
+        public string IncrementId(string id)
+        {
+            // Assuming the format is always "ABC-XXXXXX"
+            int numericPart = int.Parse(id.Substring(4));
+            int nextNumericPart = numericPart + 1;
+
+            // Make sure the nextNumericPart stays within the range of 0 to 999999
+            nextNumericPart %= 1000000;
+
+            string nextId = $"ABC-{nextNumericPart:D6}";
+
+            return nextId;
+        }
+        public async Task<string> GetNextIdEmpAsync() // Mark it as async and return Task<string>
+        {
+            var matchingIds = await _context.HospitalHealthInsurances
+                .Select(entity => entity.InsuranceId)
+                .ToListAsync(); // Remove the extra dots here
+
+            var highestId = matchingIds
+                .Where(id => id.StartsWith("Emp-"))
+                .DefaultIfEmpty("Emp-000000")
+                .Max();
+
+            var nextId = IncrementIdEmp(highestId);
+
+            return nextId;
+        }
+
+        public string IncrementIdEmp(string id)
+        {
+            // Assuming the format is always "ABC-XXXXXX"
+            int numericPart = int.Parse(id.Substring(4));
+            int nextNumericPart = numericPart + 1;
+
+            // Make sure the nextNumericPart stays within the range of 0 to 999999
+            nextNumericPart %= 1000000;
+
+            string nextId = $"Emp-{nextNumericPart:D6}";
+
+            return nextId;
+        }
+        public async Task<string> GetNextIdAdminAsync() // Mark it as async and return Task<string>
+        {
+            var matchingIds = await _context.HospitalHealthInsurances
+                .Select(entity => entity.InsuranceId)
+                .ToListAsync(); // Remove the extra dots here
+
+            var highestId = matchingIds
+                .Where(id => id.StartsWith("Admin-"))
+                .DefaultIfEmpty("Admin-000000")
+                .Max();
+
+            var nextId = IncrementIdAdmin(highestId);
+
+            return nextId;
+        }
+
+        public string IncrementIdAdmin(string id)
+        {
+            // Assuming the format is always "ABC-XXXXXX"
+            int numericPart = int.Parse(id.Substring(5));
+            int nextNumericPart = numericPart + 1;
+
+            // Make sure the nextNumericPart stays within the range of 0 to 999999
+            nextNumericPart %= 1000000;
+
+            string nextId = $"Admin-{nextNumericPart:D6}";
+
+            return nextId;
+        }
+    }
+}
